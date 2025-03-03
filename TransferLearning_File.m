4fce25f4f4c79f7aa5c2ce4f9043b100ffe1f65f
DyutideeptaB <dyutideepta.banerjee@gmail.com>
2025-03-03 20:06:52 +0530

    Add Finalised Files

---
+++ TransferLearning_File.m
@@ -0,0 +1,116 @@
+function [classes, predictedLabels_Test, score_Test, TestLabels, accuracy_Test, mynetOSAug, info] =... 
+    TransferLearning_File(path, lgraph, patchSize, chnr, epochs, MiniBatchSize, InitialLearnRate,...
+    LearnRateSchedule, LearnRateDropFactor, LearnRateDropPeriod, Shuffle, ValidationPatience,...
+    GradientThreshold, ExecutionEnvironment)
+
+%% Set up our training data
+
+ImageLocation = path; %define the location the main folder names Patches
+
+allImages = imageDatastore(ImageLocation, 'IncludeSubfolders', true,...
+    'LabelSource', 'foldernames', 'FileExtensions', '.img', 'ReadFcn', @readPatchWithProcessing);
+classes = unique(allImages.Labels);
+numClasses = numel(classes);
+
+%% Creating the validation (15 %), testing (15%) and training image dataset (70 %)
+[trainingImages, ValidationImages, TestImages] = splitEachLabel(allImages, 0.7, 0.15, 0.15, "randomized");
+
+%% Oversampling sinuous rille images (minority classes) within the training data only
+idx = trainingImages.Labels == ['sinuous-rille-' num2str(patchSize) '-' num2str(patchSize)];
+rilleds = subset(trainingImages,idx);
+
+if patchSize == 25 %Oversampled 9 times
+    trainingImagesOS = imageDatastore(cat(1, trainingImages.Files, rilleds.Files, rilleds.Files, rilleds.Files,...
+    rilleds.Files, rilleds.Files, rilleds.Files, rilleds.Files, rilleds.Files, rilleds.Files),...
+    'FileExtensions','.img', 'ReadFcn',@readPatchWithProcessing);
+    trainingImagesOS.Labels = cat(1, trainingImages.Labels, rilleds.Labels, rilleds.Labels, rilleds.Labels,...
+    rilleds.Labels, rilleds.Labels, rilleds.Labels, rilleds.Labels, rilleds.Labels, rilleds.Labels);
+end
+
+if patchSize == 50 %Oversampled 7 times
+    trainingImagesOS = imageDatastore(cat(1, trainingImages.Files, rilleds.Files, rilleds.Files, rilleds.Files,...
+    rilleds.Files, rilleds.Files, rilleds.Files, rilleds.Files),...
+    'FileExtensions','.img', 'ReadFcn',@readPatchWithProcessing);
+    trainingImagesOS.Labels = cat(1, trainingImages.Labels, rilleds.Labels, rilleds.Labels, rilleds.Labels,...
+    rilleds.Labels, rilleds.Labels, rilleds.Labels, rilleds.Labels);
+end
+
+if patchSize == 100 %Oversampled 5 times
+    trainingImagesOS = imageDatastore(cat(1, trainingImages.Files, rilleds.Files, rilleds.Files, rilleds.Files,...
+    rilleds.Files, rilleds.Files),...
+    'FileExtensions','.img', 'ReadFcn',@readPatchWithProcessing);
+    trainingImagesOS.Labels = cat(1, trainingImages.Labels, rilleds.Labels, rilleds.Labels, rilleds.Labels,...
+    rilleds.Labels, rilleds.Labels);
+end
+
+if patchSize == 200 %Oversampled 2 times
+    trainingImagesOS = imageDatastore(cat(1, trainingImages.Files, rilleds.Files, rilleds.Files),...
+    'FileExtensions','.img', 'ReadFcn',@readPatchWithProcessing);
+    trainingImagesOS.Labels = cat(1, trainingImages.Labels, rilleds.Labels, rilleds.Labels);
+end
+
+if patchSize == 500 %Oversampled 1 times
+    trainingImagesOS = imageDatastore(cat(1, trainingImages.Files, rilleds.Files),...
+    'FileExtensions','.img', 'ReadFcn',@readPatchWithProcessing);
+    trainingImagesOS.Labels = cat(1, trainingImages.Labels, rilleds.Labels);
+end
+
+%% Create augmented datastore for the oversampled training image datastore
+imageAugmenter = imageDataAugmenter('RandRotation',[-10,10]);
+imageSize = [patchSize patchSize chnr];
+trainingImagesOSAug = augmentedImageDatastore(imageSize, trainingImagesOS, "DataAugmentation", imageAugmenter);
+ValidationImagesAug = augmentedImageDatastore(imageSize, ValidationImages, "DataAugmentation", imageAugmenter);
+TestImagesAug = augmentedImageDatastore(imageSize, TestImages, "DataAugmentation", imageAugmenter);
+
+%% Creating The Network Parameters
+ValFrequency = floor(numel(trainingImagesOSAug.Files)/ MiniBatchSize);
+
+% Defining the options for training the network
+opts = trainingOptions('adam', 'InitialLearnRate', InitialLearnRate,...
+    'LearnRateSchedule', LearnRateSchedule,...
+    'LearnRateDropFactor', LearnRateDropFactor,...
+    'LearnRateDropPeriod', LearnRateDropPeriod,...
+    'GradientThreshold', GradientThreshold,...
+    'MaxEpochs', epochs,...
+    'MiniBatchSize', MiniBatchSize,...
+    'ExecutionEnvironment',ExecutionEnvironment, ...
+    'Shuffle',Shuffle, ...
+    'ValidationData', ValidationImagesAug, ...
+    'ValidationFrequency', ValFrequency, ...
+    'ValidationPatience', ValidationPatience,...
+     Plots = 'training-progress', Verbose = true);
+
+%% Changing the input and output layers of the pre-trained networks
+
+lgraph = layerGraph(lgraph);
+
+% Replacing Input Layer
+inLayer = imageInputLayer([patchSize patchSize chnr]);
+name1 = lgraph.Layers(1).Name;
+lgraph = replaceLayer(lgraph, name1, inLayer);
+
+% Choosing the layers to be changed
+names = {lgraph.Layers(end-2).Name, lgraph.Layers(end-1).Name, lgraph.Layers(end).Name};
+lgraph = removeLayers(lgraph, names);
+lastLayers = [
+    fullyConnectedLayer(numClasses,'Name','fc','BiasLearnRateFactor',10)
+    softmaxLayer('Name','softmax')
+    classificationLayer('Name','classoutput')];
+
+% Adding the new layers
+lgraph = addLayers(lgraph,lastLayers);
+
+% Connceting the new layers to the existing network
+nameJoin = {lgraph.Layers(end-3).Name, lgraph.Layers(end-2).Name};
+lgraph = connectLayers(lgraph,nameJoin{:});
+
+
+%% Calling the Network to run
+[mynetOSAug, info] = trainNetwork(trainingImagesOSAug, lgraph, opts); %defining network and the information to be printed with it
+
+%% Labels
+TestLabels = TestImages.Labels;
+
+%% Measure network accuracy on test data
+[predictedLabels_Test, score_Test] = classify(mynetOSAug, TestImages);
+accuracy_Test = mean(predictedLabels_Test == TestLabels);
---
+++ TransferLearning_Run-File.m
@@ -0,0 +1,81 @@
+%% Define all variables and run the training file
+global patchSize
+global currentCombination
+global chnr
+
+%% Uncomment the bands you want to select.
+
+%% For MI
+% bandnr = 2:10;
+% chnr = 9; % write the number of vis-nir channels we want to arbitrarily select
+% vis_nir_combs = nchoosek(bandnr, chnr);
+% for i = height(vis_nir_combs)
+%     currentCombination = [1 vis_nir_combs(i,:)];
+% end
+
+%% For RGB type
+currentCombination = [1, 5, 10]; % 3 Channels
+chnr = length(currentCombination);
+
+patchSizes = [100, 200];
+addpath('./Networks');
+networkNames = {'resnet50' 'googlenet'};    
+epochs = 30;
+InitialLearnRate = 0.001; 
+LearnRateSchedule = "piecewise";
+LearnRateDropFactor = 0.2;
+LearnRateDropPeriod = 5;
+Shuffle = 'every-epoch';
+ValidationPatience = 5;
+GradientThreshold = Inf; %default value for all the other networks
+ExecutionEnvironment = 'gpu';
+
+%Create directory for saving the training plots into
+
+myDir1 = ['TestResults_TL_' num2str(chnr) 'C']; 
+myDir2 = 'TrainingPlots';
+myDir3 = 'Data';
+if ~exist(myDir1, 'dir')
+    mkdir(['./' myDir1 '/' myDir2])
+    mkdir(['./' myDir1 '/' myDir3])
+end
+
+%% Run the training for the different networks across all patch sizes
+
+for patchSizeNr = 1:length(patchSizes)
+    patchSize = patchSizes(patchSizeNr);
+    path = ['C:\Patches\Patches' num2str(patchSize)];
+
+    if patchSize == 200
+        MiniBatchSize = 64;
+    elseif patchSize == 100
+        MiniBatchSize = 64;
+    else
+        MiniBatchSize = 50;
+    end
+
+    for networkNameNr = 1:length(networkNames)
+        try
+            networkName = networkNames{networkNameNr};
+            if ~exist(['./' myDir1 '/' myDir3 '/' networkName num2str(patchSize) '.mat'],'file')
+                disp([networkName num2str(patchSize) ': starting!']);
+            lgraph = feval(networkName);
+            [classes, predictedLabels_Test, score_Test, TestLabels, accuracy_Test, mynetOSAug, info] =...
+                TransferLearning_File(path, lgraph, patchSize, chnr, epochs, MiniBatchSize, InitialLearnRate,...
+                LearnRateSchedule, LearnRateDropFactor, LearnRateDropPeriod, Shuffle, ValidationPatience,...
+                GradientThreshold, ExecutionEnvironment);
+            save(['./' myDir1 '/' myDir3 '/' networkName num2str(patchSize) '.mat'], '-v7.3');
+            currentfig = findall(groot, 'Tag', 'NNET_CNN_TRAININGPLOT_UIFIGURE');
+            savefig(currentfig,['./' myDir1 '/' myDir2 '/' networkName num2str(patchSize) '.fig']);
+            % saveas(currentfig,['./' myDir '/' networkName num2str(patchSize) '.png']);
+            close(currentfig);
+            gpuDevice(1);
+            else
+                disp([networkName num2str(patchSize) ': skipping!']);
+            end
+
+        catch ME
+            disp([networkName num2str(patchSize) ' is not working! ' ME.message]);
+        end
+    end
+end
